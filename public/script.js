1ab5cdb916248972080c0a34e0d9d3e728e0bfd0
mytvDor <159150094+mytvDor@users.noreply.github.com>
2024-07-31 21:00:20 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,8 +3,28 @@ const startButton = document.getElementById("start-btn");
 
 const state = { media: null };
 const socket = io();
+let strcode = "YOUR_YT_CODE"
+startButton.addEventListener("click", async () => {
+  const rtmpUrl = `rtmp://a.rtmp.youtube.com/live2/${strcode}`;
 
-startButton.addEventListener("click", () => {
+  try {
+    const response = await fetch("http://localhost:4000/api", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ rtmpUrl }),
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    console.log("RTMP URL sent to server successfully");
+  } catch (error) {
+    console.error("Failed to send RTMP URL to server:", error);
+  }
+
   const mediaRecorder = new MediaRecorder(state.media, {
     audioBitsPerSecond: 128000,
     videoBitsPerSecond: 2500000,
