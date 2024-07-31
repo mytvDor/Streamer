9223b5be9edbb37aaf853c2d357de33d2fd22e07
sumit <mytvdor@gmail.com>
2024-07-31 18:40:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,73 @@
+import http from "http";
+import path from "path";
+import { spawn } from "child_process";
+import express from "express";
+import { Server as SocketIO } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIO(server);
+
+const options = [
+  "-i",
+  "-",
+  "-c:v",
+  "libx264",
+  "-preset",
+  "ultrafast",
+  "-tune",
+  "zerolatency",
+  "-r",
+  `${25}`,
+  "-g",
+  `${25 * 2}`,
+  "-keyint_min",
+  25,
+  "-crf",
+  "25",
+  "-pix_fmt",
+  "yuv420p",
+  "-sc_threshold",
+  "0",
+  "-profile:v",
+  "main",
+  "-level",
+  "3.1",
+  "-c:a",
+  "aac",
+  "-b:a",
+  "128k",
+  "-ar",
+  128000 / 4,
+  "-f",
+  "flv",
+  `rtmp://a.rtmp.youtube.com/live2/wk7e-b2gd-ymdu-1fee-cvuc`,
+];
+
+const ffmpegProcess = spawn("ffmpeg", options);
+
+ffmpegProcess.stdout.on("data", (data) => {
+  console.log(`ffmpeg stdout: ${data}`);
+});
+
+ffmpegProcess.stderr.on("data", (data) => {
+  console.error(`ffmpeg stderr: ${data}`);
+});
+
+ffmpegProcess.on("close", (code) => {
+  console.log(`ffmpeg process exited with code ${code}`);
+});
+
+app.use(express.static(path.resolve("./public")));
+
+io.on("connection", (socket) => {
+  console.log("Socket Connected", socket.id);
+  socket.on("binarystream", (stream) => {
+    console.log("Binary Stream Incommming...");
+    ffmpegProcess.stdin.write(stream, (err) => {
+      if (err) console.log("Err", err);
+    });
+  });
+});
+
+server.listen(3000, () => console.log(`HTTP Server is runnning on PORT 3000`));
